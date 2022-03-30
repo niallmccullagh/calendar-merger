333dd440cbc3e64d0fc093555dfc7e324ec5adf4
Niall McCullagh <niallmccullagh@users.noreply.github.com>
2022-03-30 20:17:54 +0100

    Initial commit

---
+++ test/calendar-merge.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CalendarMerge = require('../lib/calendar-merge-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/calendar-merge-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CalendarMerge.CalendarMergeStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
